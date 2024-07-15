d16879c069ad6701089dda332aff6e9a24d52b79
Atil Samancioglu <samancioglu.atil@gmail.com>
2024-07-15 18:54:52 +0300

    bugs fixed

--- ingestion.py
+++ ingestion.py
@@ -20,12 +20,12 @@ text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
 )
 doc_splits = text_splitter.split_documents(docs_list)
 
-# vectorstore = Chroma.from_documents(
-#     documents=doc_splits,
-#     collection_name="rag-chroma",
-#     embedding=OpenAIEmbeddings(),
-#     persist_directory="./.chroma",
-# )
+vectorstore = Chroma.from_documents(
+     documents=doc_splits,
+     collection_name="rag-chroma",
+     embedding=OpenAIEmbeddings(),
+     persist_directory="./.chroma",
+ )
 
 retriever = Chroma(
     collection_name="rag-chroma",
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+from graph.graph import app
 
-question1 = "What are the types of agent memory?"
-inputs = {"question": question1}
-
+if __name__ == "__main__":
+    print("Hello Advanced RAG")
+    print(app.invoke(input={"question": "agent memory?"}))
